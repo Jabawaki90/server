678aec28efe613cbb29a14e223cf4a487eae3165
Ashraf Azemi <ashraf.chzm@gmail.com>
2023-07-25 23:53:07 +0800

    connecting front end

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
+app.use(cors());
 
-const server = app;
+// app.get("/api", (req, res) => {
+//   res.json({ users: "user 1" });
+//   res.send(200);
+// });
+
+app.get("/getData", (req, res) => {
+  res.send("Hello");
+});
+
+app.listen(3001, () => {
+  console.log("Hello Note project");
+});
